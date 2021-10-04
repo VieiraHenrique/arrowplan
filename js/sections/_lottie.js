7c110d2664ed19f7ed9dcfa06bb5fbef0b470731
Henrique Vieira <henrique@mvstudio.be>
2021-10-04 17:46:59 +0200

    wip

--- js/sections/_lottie.js
+++
@@ -1,9 +0,0 @@
-const world = document.getElementById('world');
-
-lottie.loadAnimation({
-    container: world, // the dom element that will contain the animation
-    renderer: 'svg',
-    loop: true,
-    autoplay: true,
-    path: '/lottie/world.json' // the path to the animation json
-  });
--- js/sections/_menu.js
+++ js/sections/_menu.js
@@ -1,12 +1,15 @@
-const header = document.getElementById("header");
+const body = document.querySelector("body");
+const hamburger = document.querySelector(".hamburger");
+const menuItems = document.querySelectorAll('.header__nav--menu a');
+const toggle = document.getElementById('toggle');
 
-window.onscroll = function () {
-  if (
-    document.body.scrollTop > 130 ||
-    document.documentElement.scrollTop > 130
-  ) {
-    header.classList.add("small");
-  } else {
-    header.classList.remove("small");
-  }
-};
+hamburger.addEventListener("click", () => {
+  body.classList.toggle("noscroll");
+});
+
+menuItems.forEach((item)=>{
+  item.addEventListener('click', ()=>{
+    body.classList.remove('noscroll');
+    toggle.checked = false;
+  })
+})
